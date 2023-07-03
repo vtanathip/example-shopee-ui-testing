7e4c7aa581651fe7a01a791993eb40eef2d95e5d
LABTOP-VTANATHIP <tanathip.se@gmail.com>
2023-07-03 14:18:18 +0700

    Added first test case for checking name on menubar

--- test/pageobjects/main.page.ts
+++ test/pageobjects/main.page.ts
@@ -4,17 +4,50 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class MainPage extends Page {
+    
+    private _languageButtonSelector: string = "";
+
     /**
      * define selectors using getter methods
      */
+    public get languageButton(){
+        return $(this._languageButtonSelector);
+    }
 
+    public get startSellingButton(){
+        return $('nav > div.flex > a:nth-child(2)');
+    }
+
+    public get bannerModal(): ChainablePromiseElement{
+        return $('div[class="home-page"] > shopee-banner-popup-stateful')
+    }
+
+    public get bannerCloseButton(): string{
+        return 'div[aria-label="Close"'
+    }
+
+    /**
+     * define selectors using setter methods
+     */
+    private set chooseLangauge(index: number){
+        this._languageButtonSelector = `div[class="language-selection__list"] > div:nth-child(${index}) > button`;
+    }
 
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    public async language (language: string) {
-        
+    public async switchLanguage (language: number) {
+        this.chooseLangauge = language;
+        await (await this.languageButton).click();
+    }
+    
+    public async checkStartSellingButton(): Promise<string> {
+        return await (await this.startSellingButton).getText();
+    }
+
+    public async closeBanner(): Promise<void> {
+        await (await (await this.bannerModal).shadow$(this.bannerCloseButton)).click()
     }
 
     /**
--- test/specs/shopee.e2e.ts
+++ test/specs/shopee.e2e.ts
@@ -8,9 +8,16 @@ describe('Shopee UI testing', () => {
     })
 
     it('Check language change', async () => {
+        let expectedText = 'Start Selling';
+        let actualText = '';
+        await MainPage.switchLanguage(2);
+        await MainPage.closeBanner();
+        actualText = await MainPage.checkStartSellingButton();
+        expect(actualText).toEqual(expectedText);
     })
     
     it('Validate help category', async () => {
+
     })
     
     it('Search for most expensive toy items', async () => {
